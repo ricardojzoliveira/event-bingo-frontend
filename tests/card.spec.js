833ba454f6cd5b5795fcceeaa81097162b0cd7d2
Ricardo Oliveira <rychard.oliveira2005@gmail.com>
2026-06-18 02:27:29 +0100

    create, edit and delete card tests

---
+++ tests/card.spec.js
@@ -0,0 +1,87 @@
+import { test, expect } from '@playwright/test';
+
+test('create-card', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'Enter your username' }).fill('admin');
+  await page.getByRole('textbox', { name: '••••••••' }).fill('123');
+  await page.getByRole('button', { name: 'Sign In' }).click();
+  
+  await page.getByRole('link', { name: 'Admin Panel' }).click();
+  await page.getByRole('link', { name: 'Cards Management' }).click();
+  await page.getByRole('link', { name: 'Create Card' }).click();
+  
+  await page.getByRole('textbox', { name: 'Ex: Weekend Specials' }).fill('Test Card');
+  await page.getByPlaceholder('10.00').fill('10');
+  await page.getByPlaceholder('50', { exact: true }).fill('100');
+  await page.getByPlaceholder('500').fill('1000');
+
+  for (let i = 0; i < 9; i++) {
+    const availabeEvent = page.locator('.cursor-grab').first();
+    const goalCell = page.locator('div.aspect-square').nth(i);
+
+    await availabeEvent.dragTo(goalCell);
+    await expect(goalCell.getByText('VS')).toBeVisible({ timeout: 2000 });
+  }
+
+  await page.getByRole('button', { name: 'Publish Card' }).click();
+
+  await expect(page).toHaveURL('http://localhost:5173/admin/cards');
+
+  const newCard = page.getByRole('heading', { name: 'Test Card' }).first();
+  
+  await expect(newCard).toBeVisible({ timeout: 5000 });
+});
+
+test('edit-card', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'Enter your username' }).fill('admin');
+  await page.getByRole('textbox', { name: '••••••••' }).fill('123');
+  await page.getByRole('button', { name: 'Sign In' }).click();
+  
+  await page.getByRole('link', { name: 'Admin Panel' }).click();
+  await page.getByRole('link', { name: 'Cards Management' }).click();
+  
+  const firstCardItem = page.locator('div.group.relative').first();
+  
+  await firstCardItem.locator('a[href*="/admin/cards/edit/"]').click();
+  
+  await page.getByRole('textbox', { name: 'Ex: Weekend Specials' }).fill('Test Edited');
+  await page.getByPlaceholder('10.00').fill('15');
+  await page.getByPlaceholder('50', { exact: true }).fill('150');
+  await page.getByPlaceholder('500').fill('1500');
+  
+  await page.getByRole('button', { name: 'Update Card' }).click();
+  
+  await expect(page).toHaveURL('http://localhost:5173/admin/cards');
+  await expect(page.getByRole('heading', { level: 3, name: 'Test Edited' }).first()).toBeVisible({ timeout: 5000 });
+});
+
+
+test('delete-card', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'Enter your username' }).fill('admin');
+  await page.getByRole('textbox', { name: '••••••••' }).fill('123');
+  await page.getByRole('button', { name: 'Sign In' }).click();
+  
+  await page.getByRole('link', { name: 'Admin Panel' }).click();
+  await page.getByRole('link', { name: 'Cards Management' }).click();
+  
+  const firstCardItem = page.locator('div.group.relative').first();
+  const cardName = await firstCardItem.getByRole('heading', { level: 3 }).innerText();
+  
+  page.once('dialog', async (dialog) => {
+    console.log(`Delete Card: ${dialog.message()}`);
+    await dialog.accept();
+  });
+  
+  await firstCardItem.locator('button').last().click();
+  
+  const cardApagadoHeading = page.getByRole('heading', { level: 3, name: cardName });
+  await expect(cardApagadoHeading).not.toBeVisible({ timeout: 5000 });
+});
